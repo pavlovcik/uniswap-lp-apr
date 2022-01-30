29a89bbfb207f0b08bcd3fc54de8e7a6fd9e8bb6
アレクサンダー.eth <gpg@pavlovcik.com>
2022-01-30 04:17:04 -0600

    add(files): forgot to add to previous commit

--- src/calculateAPR.ts
+++ src/calculateAPR.ts
@@ -1,5 +1,5 @@
-import { attachMutationObserver } from "./utils";
-import { getPositionIdFromUrl, readLocalStorage, setupDomNode, updateDomNode, writeLocalStorage } from "./utils/common";
+import { callback } from "./callback";
+import { getPositionIdFromUrl, readLocalStorage, setupDomNode } from "./utils/common";
 import { queryBlockchain } from "./utils/query-blockchain";
 
 export const MS_IN_YEAR = 31536000000;
@@ -14,56 +14,8 @@ export const state = {
 	domNode: setupDomNode(),
 } as any;
 
-const POSITION_ID = getPositionIdFromUrl();
+export const POSITION_ID = getPositionIdFromUrl();
 const queryTimestamp = `{"query": "{positions(where: {id: ${POSITION_ID}}) {transaction {timestamp}}}"}`;
-interface timestampResponse {
-	data: {
-		positions: { transaction: { timestamp: string } }[];
-	};
-}
+
 
 queryBlockchain(queryTimestamp).then(callback);
-
-function callback(timestamp: any) {
-	console.log(JSON.stringify(timestamp));
-
-	const CAPTURED = document.body.innerText.match(/^\$[0-9]{1,9}([,.][0-9]{1,9})/gim);
-	if (!CAPTURED) {
-		throw new Error("No captured found");
-	}
-
-	state.liquidity = parseFloat(CAPTURED[0].replace(",", "").slice(1));
-	state.fees = parseFloat(CAPTURED[1].replace(",", "").slice(1));
-	state.percentYield = state.fees / state.liquidity;
-
-	let depositTime;
-	if (state.storage[POSITION_ID]) {
-		depositTime = state.storage[POSITION_ID];
-	} else {
-		state.storage[POSITION_ID] = depositTime = prompt("Paste the deposit time here");
-	}
-
-	const NOW = new Date();
-	const DEPOSIT_TIME_LOCAL = new Date(depositTime);
-
-	const TIMEZONE_OFFSET = DEPOSIT_TIME_LOCAL.getTimezoneOffset() * 60000;
-	state.depositTime = new Date(DEPOSIT_TIME_LOCAL.getTime() - TIMEZONE_OFFSET);
-	state.timeElapsed = NOW.getTime() - state.depositTime.getTime();
-	state.projectedAPR = state.percentYield / (state.timeElapsed / MS_IN_YEAR);
-
-	console.log(state);
-	updateDomNode(
-		`${state.projectedAPR} · APR\n$${((state.projectedAPR / 365) * state.liquidity).toFixed(2)} · Daily\n$${(state.projectedAPR * state.liquidity).toFixed(
-			2
-		)} · Annual`
-	);
-
-	const newDepositTime = prompt(`APR: ${state.projectedAPR}\nPaste in a new deposit time to update.`);
-
-	if (newDepositTime) {
-		state.storage[POSITION_ID] = newDepositTime;
-		writeLocalStorage();
-	}
-
-	attachMutationObserver();
-}
---
+++ src/utils/calculate-timings.ts
@@ -0,0 +1,27 @@
+import { state, POSITION_ID, MS_IN_YEAR } from "../calculateAPR";
+import { Timestamp } from "../callback";
+
+export function calculateTimings(timestamp: unknown) {
+    let depositTime;
+    if (state.storage[POSITION_ID]) {
+        depositTime = state.storage[POSITION_ID];
+    } else {
+        try {
+            // 1639349303 = 1970-01-19T23:22:29.303Z
+            // 1639349303000 = 2021-12-12T22:48:23.000Z
+            const ts = parseInt((timestamp as Timestamp).data.positions[0].transaction.timestamp.concat(`000`));
+            state.storage[POSITION_ID] = depositTime = ts;
+        } catch (err) {
+            console.error(err);
+            state.storage[POSITION_ID] = depositTime = prompt("Paste the deposit time here");
+        }
+    }
+
+    const NOW = new Date();
+    const DEPOSIT_TIME_LOCAL = new Date(depositTime);
+
+    const TIMEZONE_OFFSET = DEPOSIT_TIME_LOCAL.getTimezoneOffset() * 60000;
+    state.depositTime = new Date(DEPOSIT_TIME_LOCAL.getTime() - TIMEZONE_OFFSET);
+    state.timeElapsed = NOW.getTime() - state.depositTime.getTime();
+    state.projectedAPR = state.percentYield / (state.timeElapsed / MS_IN_YEAR);
+}
