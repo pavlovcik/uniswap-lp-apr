54111fdc5e884a3f0573540455adc700889441e4
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-11 13:22:02 +0800

    fix(livejs): convert to ts version

--- utils/livejs-fork.ts
+++ utils/livejs-fork.ts
@@ -1,48 +1,49 @@
+/* eslint-disable no-fallthrough */
 /*
-  Live.js - One script closer to Designing in the Browser
-  Written for Handcraft.com by Martin Kool (@mrtnkl).
+ Live.js - One script closer to Designing in the Browser
+ Written for Handcraft.com by Martin Kool (@mrtnkl).
 
-  Version 4.
-  Recent change: Made stylesheet and mimetype checks case insensitive.
+ Version 4.
+ Recent change: Made stylesheet and mimetype checks case insensitive.
 
-  http://livejs.com
-  http://livejs.com/license (MIT)
-  @livejs
+ http://livejs.com
+ http://livejs.com/license (MIT)
+ @livejs
 
-  Include live.js#css to monitor css changes only.
-  Include live.js#js to monitor js changes only.
-  Include live.js#html to monitor html changes only.
-  Mix and match to monitor a preferred combination such as live.js#html,css
+ Include live.js#css to monitor css changes only.
+ Include live.js#js to monitor js changes only.
+ Include live.js#html to monitor html changes only.
+ Mix and match to monitor a preferred combination such as live.js#html,css
 
-  By default, just include live.js to monitor all css, js and html changes.
+ By default, just include live.js to monitor all css, js and html changes.
 
-  Live.js can also be loaded as a bookmarklet. It is best to only use it for CSS then,
-  as a page reload due to a change in html or css would not re-include the bookmarklet.
-  To monitor CSS and be notified that it has loaded, include it as: live.js#css,notify
+ Live.js can also be loaded as a bookmarklet. It is best to only use it for CSS then,
+ as a page reload due to a change in html or css would not re-include the bookmarklet.
+ To monitor CSS and be notified that it has loaded, include it as: live.js#css,notify
 */
-(function () {
-	let headers = { Etag: 1, "Last-Modified": 1, "Content-Length": 1, "Content-Type": 1 },
+export default function liveJs() {
+	const headers = { Etag: 1, "Last-Modified": 1, "Content-Length": 1, "Content-Type": 1 },
 		resources = {},
 		pendingRequests = {},
 		currentLinkElements = {},
 		oldLinkElements = {},
 		interval = 1000,
-		loaded = false,
 		active = { html: 1, css: 1, js: 1 };
+	let loaded = false;
 
-	var Live = {
+	const Live = {
 		// performs a cycle per interval
 		heartbeat: function () {
 			if (document.body) {
 				// make sure all resources are loaded on first activation
-				if (!loaded) Live.loadresources();
+				if (!loaded) Live.loadResources();
 				Live.checkForChanges();
 			}
 			setTimeout(Live.heartbeat, interval);
 		},
 
 		// loads all local css and js resources upon first activation
-		loadresources: function () {
+		loadResources: function () {
 			// helper method to assert if a given url is local
 			function isLocal(url) {
 				const loc = document.location,
@@ -51,12 +52,13 @@
 			}
 
 			// gather all resources
-			let scripts = document.getElementsByTagName("script"),
-				links = document.getElementsByTagName("link"),
-				uris = [];
+			const scripts = document.getElementsByTagName("script"),
+				links = document.getElementsByTagName("link");
+
+			let uris = [] as string[];
 
 			// track local js urls
-			for (var i = 0; i < scripts.length; i++) {
+			for (let i = 0; i < scripts.length; i++) {
 				const script = scripts[i],
 					src = script.getAttribute("src");
 				if (src && isLocal(src)) uris.push(src);
@@ -69,10 +71,10 @@
 			if (active.html) uris.push(document.location.href);
 
 			// track local css urls
-			for (var i = 0; i < links.length && active.css; i++) {
+			for (let i = 0; i < links.length && active.css; i++) {
 				const link = links[i],
 					rel = link.getAttribute("rel"),
-					href = link.getAttribute("href", 2);
+					href = link.getAttribute("href");
 				if (href && rel && rel.match(new RegExp("stylesheet", "i")) && isLocal(href)) {
 					uris.push(href);
 					currentLinkElements[href] = link;
@@ -80,7 +82,7 @@
 			}
 
 			// initialize the resources info
-			for (var i = 0; i < uris.length; i++) {
+			for (let i = 0; i < uris.length; i++) {
 				const url = uris[i];
 				Live.getHead(url, function (url, info) {
 					resources[url] = info;
@@ -91,10 +93,10 @@
 			const head = document.getElementsByTagName("head")[0],
 				style = document.createElement("style"),
 				rule = "transition: all .3s ease-out;";
-			css = [".livejs-loading * { ", rule, " -webkit-", rule, "-moz-", rule, "-o-", rule, "}"].join("");
+			const css = [".livejs-loading * { ", rule, " -webkit-", rule, "-moz-", rule, "-o-", rule, "}"].join("");
 			style.setAttribute("type", "text/css");
 			head.appendChild(style);
-			style.styleSheet ? (style.styleSheet.cssText = css) : style.appendChild(document.createTextNode(css));
+			style.appendChild(document.createTextNode(css));
 
 			// yep
 			loaded = true;
@@ -106,8 +108,8 @@
 				if (pendingRequests[url]) continue;
 
 				Live.getHead(url, function (url, newInfo) {
-					let oldInfo = resources[url],
-						hasChanged = false;
+					const oldInfo = resources[url];
+					let hasChanged = false;
 					resources[url] = newInfo;
 					for (const header in oldInfo) {
 						// do verification based on the header type
@@ -137,22 +139,23 @@
 			switch (type.toLowerCase()) {
 				// css files can be reloaded dynamically by replacing the link element
 				case "text/css":
-					var link = currentLinkElements[url],
-						html = document.body.parentNode,
+					// eslint-disable-next-line no-case-declarations
+					const link = currentLinkElements[url],
+						body = document.body,
 						head = link.parentNode,
 						next = link.nextSibling,
 						newLink = document.createElement("link");
 
-					html.className = html.className.replace(/\s*livejs\-loading/gi, "") + " livejs-loading";
+					body.className = body.className.replace(/\s*livejs-loading/gi, "") + " livejs-loading";
 					newLink.setAttribute("type", "text/css");
 					newLink.setAttribute("rel", "stylesheet");
-					newLink.setAttribute("href", url + "?now=" + new Date() * 1);
+					newLink.setAttribute("href", url + "?now=" + Date.now());
 					next ? head.insertBefore(newLink, next) : head.appendChild(newLink);
 					currentLinkElements[url] = newLink;
 					oldLinkElements[url] = link;
 
 					// schedule removal of the old link
-					Live.removeoldLinkElements();
+					Live.removeOldLinkElements();
 					break;
 
 				// check if an html resource is our current url, then reload
@@ -168,27 +171,27 @@
 		},
 
 		// removes the old stylesheet rules only once the new one has finished loading
-		removeoldLinkElements: function () {
+		removeOldLinkElements: function () {
 			let pending = 0;
 			for (const url in oldLinkElements) {
 				// if this sheet has any cssRules, delete the old link
 				try {
-					var link = currentLinkElements[url],
+					const link = currentLinkElements[url],
 						oldLink = oldLinkElements[url],
-						html = document.body.parentNode,
+						body = document.body,
 						sheet = link.sheet || link.styleSheet,
 						rules = sheet.rules || sheet.cssRules;
 					if (rules.length >= 0) {
 						oldLink.parentNode.removeChild(oldLink);
 						delete oldLinkElements[url];
 						setTimeout(function () {
-							html.className = html.className.replace(/\s*livejs\-loading/gi, "");
+							body.className = body.className.replace(/\s*livejs-loading/gi, "");
 						}, 100);
 					}
 				} catch (e) {
 					pending++;
 				}
-				if (pending) setTimeout(Live.removeoldLinkElements, 50);
+				if (pending) setTimeout(Live.removeOldLinkElements, 50);
 			}
 		},
 
@@ -219,7 +222,12 @@
 	// start listening
 	if (document.location.protocol != "file:") {
 		if (!window.liveJsLoaded) Live.heartbeat();
-
 		window.liveJsLoaded = true;
 	} else if (window.console) console.log("Live.js doesn't support the file protocol. It needs http.");
-})();
+}
+
+declare global {
+	interface Window {
+		liveJsLoaded: boolean;
+	}
+}
