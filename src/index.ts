5458c7f433dbac96c258b62b4b95596e01d779b1
アレクサンダー.eth <gpg@pavlovcik.com>
2022-07-11 18:07:39 +0800

    adjust(ci): closure compiler no longer works after adding observable hq plot so need to load normal tsc export in client

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { dom } from "./utils/dom";
 if (window.state) {
 	// a previous instance exists, so remove it
 	window.state.observer?.disconnect();
-	window.state.domNode.parentElement?.removeChild(window.state.domNode);
+	window.state.domNode?.parentElement?.removeChild(window.state.domNode);
 	delete window.state;
 }
 
@@ -15,10 +15,12 @@ const state = (window.state = new State());
 
 dom.attachMutationObserver(state); // just once
 
-main(state).finally(() => {
-	console.log(`Uniswap APR Bookmarklet loaded successfully. Version ${version}.`);
-	console.log(`Access bookmarklet state via window.state.`);
-});
+main(state)
+	.then(() => {
+		console.log(`Uniswap APR Bookmarklet loaded successfully. Version ${version}.`);
+		console.log(`Access bookmarklet state via window.state.`);
+	})
+	.catch((error) => console.error(error));
 
 declare global {
 	interface Window {
